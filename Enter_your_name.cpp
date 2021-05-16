2c25fd9bc1aed887fe6b7d3c29fa11012f270d9a
Majesa1986 <mariajsanzana@gmail.com>
2021-05-16 14:12:26 -0300

    Agregue Introduce tu nombre desde cmd

---
+++ Enter_your_name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Introduce tu nombre: ";
+    cin >> name;
+    cout << "Hola " << name << std::endl;
+
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
